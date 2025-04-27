f5810b9d02915ee657b1ad07b67ea87c29be6ca6
Logan Hurst <Loganjhurst@gmail.com>
2025-04-27 02:31:52 -0700

    Updated lighting

--- src/components/HeroModels/HeroLights.jsx
+++ src/components/HeroModels/HeroLights.jsx
@@ -6,24 +6,24 @@ const HeroLights = () => {
     <>
     <spotLight 
         position={[5,5,-1]}
-        intensity={100}
+        intensity={40}
         angle ={0.2}
         penumbra={0.2}
         color="white"
     />
         <spotLight 
         position={[4,5,5]}
-        intensity={60}
+        intensity={100}
         angle ={0.5}
         penumbra={0.7}
-        color="##07508c"
+        color="#07508c"
     />
     <spotLight 
-        position={[-3,5,5]}
-        intensity={60}
+        position={[-3,8,1]}
+        intensity={100}
         angle ={0.4}
-        penumbra={1}
-        color="#9d4edd"
+        penumbra={0.3}
+        color="#234963"
     />
     <primitive
         object={new THREE.RectAreaLight("#45078c", 8, 3, 2)}
@@ -34,12 +34,12 @@ const HeroLights = () => {
     <pointLight
         position={[0, 1, 0]}
         intensity={10}
-        color="#7209b7"
+        color="#5898bf"
     />
     <pointLight
         position={[1, 2, -2]}
-        intensity={100}
-        color="#6d078c"
+        intensity={1}
+        color="#f589b8"
     />
 
     </>
