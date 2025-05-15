5317ccb1eb2ef48b9efda168ad122c7c5de7228c
Logan Hurst <Loganjhurst@gmail.com>
2025-05-14 17:09:51 -0700

    Added experience section with components

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import ShowcaseSection from './sections/ShowcaseSection'
 import Navbar from './components/Navbar'
 import LogoSection from './sections/LogoSection'
 import FeatureCards from './sections/FeatureCards'
+import Experience from './sections/Experience'
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
     <ShowcaseSection />
     <LogoSection />
     <FeatureCards />
+    <Experience/>
     </>
     
   )
